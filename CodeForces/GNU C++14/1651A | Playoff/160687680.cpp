51681658a7c7df686bc118f79f50dcb80c04e44e
hazemkhaled241 <84819643+hazemkhaled241@users.noreply.github.com>
2022-07-04 04:06:39 +0200

    Committing files

---
+++ CodeForces/GNU C++14/1651A | Playoff/160687680.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <limits>
+#include <cmath>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void lucky(){
+long long n;
+cin>>n;
+long long result=pow(2,n)-1;
+cout<<result<<endl;
+ 
+ 
+ 
+}
+int main()
+{
+ 
+  int t;
+ 
+   cin>>t;
+    for(int i=0;i<t;i++){
+ 
+   lucky();
+    }
+ 
+    return 0;
+}
