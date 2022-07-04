f66abf065625012d51aba4b1df9f1f2635ae96b1
hazemkhaled241 <84819643+hazemkhaled241@users.noreply.github.com>
2022-07-04 04:05:51 +0200

    Committing files

---
+++ CodeForces/GNU C++14/1646A | Square Counting/161588698.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <limits>
+#include <cmath>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void lucky(){
+long long n,s,m;
+bool f=true;
+cin>>n>>s;
+ 
+ m=n*n;
+ 
+ cout<<s/m<<endl;
+ 
+}
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+ 
+  int t;
+ 
+   cin>>t;
+    for(int i=0;i<t;i++){
+ 
+   lucky();
+    }
+ 
+    return 0;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
