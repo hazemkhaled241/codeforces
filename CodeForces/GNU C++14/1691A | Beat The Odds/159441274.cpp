56303419645aa28d868b84f073fed456ce2e94d3
hazemkhaled241 <84819643+hazemkhaled241@users.noreply.github.com>
2022-07-04 04:07:41 +0200

    Committing files

---
+++ CodeForces/GNU C++14/1691A | Beat The Odds/159441274.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <limits>
+#include <string>
+using namespace std;
+ void lucky(){
+int even=0,odd=0,n;
+cin>>n;
+int arr[n];
+ for(int i=0;i<n;i++){
+    cin>>arr[i];
+    if(arr[i]%2==0)
+        even++;
+    else
+        odd++;
+    }
+ 
+ 
+ cout<<min(n-even,n-odd)<<endl;
+ }
+ 
+int main()
+{
+ 
+  int t;
+ 
+   cin>>t;
+    for(int i=0;i<t;i++){
+ 
+     lucky();
+    }
+ 
+ 
+ 
+    return 0;
+}
+//999
